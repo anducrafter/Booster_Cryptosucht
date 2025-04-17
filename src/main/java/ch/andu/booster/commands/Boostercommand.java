73dd4419e3fdc5e870e4a421ee3659c02005ca99
BuildTools <unconfigured@null.spigotmc.org>
2025-04-17 21:03:26 +0200

    für Gabso

--- src/main/java/ch/andu/booster/commands/Boostercommand.java
+++ src/main/java/ch/andu/booster/commands/Boostercommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
 import java.util.HashMap;
 import java.util.Locale;
 
